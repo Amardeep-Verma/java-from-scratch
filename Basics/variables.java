41ee9f3a2157b5c32c1ad2cc00b9f0bb01d5e543
Amardeep <vermaamardeep86@gmail.com>
2024-03-25 12:32:17 +0530

    First commit 🚀

---
+++ Basics/variables.java
@@ -0,0 +1,20 @@
+public class variables {
+    public static void main(String args[]) {
+        String name = "Rishu";
+        System.out.println(name);
+        System.out.println("Your name is " + name); // string concatenation
+        int number = 14;
+        System.out.println(number);
+
+        float fl = 34.67f;
+        System.out.println(fl);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        int myNum = 6;
+        myNum = 7;
+        System.out.println("Number after updating is : " + myNum);
+
+    }
+}
