ead8d8f3a63db73beec6f7b89325d1e48cfccdac
Amardeep <vermaamardeep86@gmail.com>
2024-03-25 15:47:18 +0530

    Basics of java

---
+++ Basics/largestOfThree.java
@@ -0,0 +1,14 @@
+public class largestOfThree {
+    public static void main(String args[]) {
+        int a = 1, b = 3, c = 6;
+
+        if ((a >= b) && (a >= c)) {
+            System.out.println("largest is A");
+        } else if (b >= c) {
+            System.out.println("largest is B ");
+        } else {
+            System.out.println("largest is C ");
+        }
+    }
+
+}
